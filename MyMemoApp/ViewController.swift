f898577b123a85ccf681a795546dfd0cc61e94f7
TaharaNoNeko <tmdjnch0901@gmail.com>
2020-08-11 11:45:21 +0900

    Initial Commit

---
+++ MyMemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemoApp
+//
+//  Created by Junichi Tomida on 2020/08/11.
+//  Copyright © 2020 infoprax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
